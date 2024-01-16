27dbf49a8457adc3d59e9cd3d642169d08837f4a
Pablo Espinoza <pablo.espinoza@posibillian.tech>
2024-01-16 15:23:59 -0300

    main changes

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   const logger = new Logger('Bootstrap');
 
   app.setGlobalPrefix('api');
