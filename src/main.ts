7c52936dcc5e95c72a482db7b22693410a678029
Pablo Espinoza <pablo.espinoza@posibillian.tech>
2024-01-16 18:06:03 -0300

    nice changes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder } from '@nestjs/swagger';
-import { SwaggerModule } from '@nestjs/swagger/dist';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  // Configuración de CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204, // Añadido para las solicitudes OPTIONS
+  });
 
   const logger = new Logger('Bootstrap');
 
@@ -24,10 +30,12 @@ async function bootstrap() {
     .setDescription('Teslo shop endpoints')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
   logger.log(`App running on port ${process.env.PORT}`);
 }
+
 bootstrap();
